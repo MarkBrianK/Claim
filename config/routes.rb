59a9f99646135a6fb7b4724cd7e263090ce6f2bb
MarkBrian <kariukimarkbrian@gmail.com>
2023-10-30 18:59:59 +0300

    :bug: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@
 Rails.application.routes.draw do
   devise_for :user, controllers: {
     sessions: 'user/sessions',
-    registrations: 'user/registrations',
+    registrations: 'user/registrations', # Ensure 'registrations' points to your custom controller.
     confirmations: 'user/confirmations'
-
   }
+
   resources :users, defaults: { format: :json }
 end
