d7a89ce4e305b5711e04a5d62c23aa607b5e0e43
MarkBrian <kariukimarkbrian@gmail.com>
2023-10-30 18:58:51 +0300

    :bug: remove . permit

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -10,24 +10,24 @@ render json:@users
   end
   def create
     build_resource(user_params)
-  
+
     if resource.save
       render json: { success: true, message: 'Signed up successfully.' }
       else
       render json: { success: false, message: resource.errors.full_messages.first }
     end
   end
-  
+
 
     # def sign_up_params
     #   params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :username, :profile_picture)
     # end
   private
   def user_params
-    params.permit(:user).permit(:first_name, :last_name, :phone, :id_or_passport, :email, :password, :password_confirmation)
+    params.permit(:first_name, :last_name, :phone, :id_or_passport, :email, :password, :password_confirmation)
   end
-  
-  
+
+
 
 
 
